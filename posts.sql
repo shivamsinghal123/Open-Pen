68b1c68ae967a55d56a082408ba8b74753bb0d9e
shivamsinghal123 <shivamsinghal@sfhs.com>
2018-06-23 19:14:41 -0700

    Update posts.sql

--- posts.sql
+++ posts.sql
@@ -5,4 +5,5 @@ post_date       DATETIME NOT NULL,
 post_topic      INT(8) NOT NULL,
 post_by     INT(8) NOT NULL,
 PRIMARY KEY (post_id)
+ALTER TABLE posts ADD FOREIGN KEY(post_topic) REFERENCES topics(topic_id) ON DELETE CASCADE ON UPDATE CASCADE;
 ) TYPE=INNODB;
